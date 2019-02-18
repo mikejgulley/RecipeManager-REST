4a63363b8f5ceb72f076de3282a82f6d2fd82225
Michael Gulley <mgulley.mograph@gmail.com>
2019-02-18 17:43:07 -0500

    Add Swagger annotations and archive/unarchive methods

--- src/main/java/com/lumatik/RecipeManagerREST/controller/RecipeController.java
+++ src/main/java/com/lumatik/RecipeManagerREST/controller/RecipeController.java
@@ -3,11 +3,13 @@ package com.lumatik.RecipeManagerREST.controller;
 import com.lumatik.RecipeManagerREST.model.Recipe;
 import com.lumatik.RecipeManagerREST.error.RecipeNotFoundException;
 import com.lumatik.RecipeManagerREST.model.RecipeResourceAssembler;
+import com.lumatik.RecipeManagerREST.model.Status;
 import com.lumatik.RecipeManagerREST.repository.RecipeRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.ResourceAssembler;
-import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,8 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping(value = "/recipes", produces = "application/json")
+@Api(value = "recipemanager", description = "Operations pertaining to recipes in Recipe Manager application")
 public class RecipeController {
 
     @Autowired
@@ -28,7 +32,8 @@ public class RecipeController {
     @Autowired
     private RecipeResourceAssembler recipeResourceAssembler;
 
-    @GetMapping("/recipes")
+    @ApiOperation(value = "View a list of recipes", response = Resources.class)
+    @GetMapping("/")
     // Resources<T> - Spring HATEOAS container that encapsulates collections of type T Resources
     // :: is Java 8 syntax for a method reference
     public Resources<Resource<Recipe>> all() {
@@ -40,7 +45,8 @@ public class RecipeController {
                 linkTo(methodOn(RecipeController.class).all()).withSelfRel());
     }
 
-    @PostMapping("/recipes")
+    @ApiOperation(value = "Add a recipe", response = ResponseEntity.class)
+    @PostMapping("/")
     ResponseEntity<?> newRecipe(@RequestBody Recipe newRecipe) throws URISyntaxException {
         Resource<Recipe> recipeResource = recipeResourceAssembler
                 .toResource(recipeRepository.save(newRecipe));
@@ -51,7 +57,8 @@ public class RecipeController {
                 .body(recipeResource);
     }
 
-    @GetMapping("/recipes/{id}")
+    @ApiOperation(value = "View a product by its id", response = Resource.class)
+    @GetMapping("/{id}")
     public Resource<Recipe> one(@PathVariable Long id) {
         Recipe recipe = recipeRepository.findById(id)
                 .orElseThrow(() -> new RecipeNotFoundException(id));
@@ -59,7 +66,8 @@ public class RecipeController {
         return recipeResourceAssembler.toResource(recipe);
     }
 
-    @PutMapping("/recipes/{id}")
+    @ApiOperation(value = "Update a recipe", response = Recipe.class)
+    @PutMapping("/{id}")
     Recipe replaceRecipe(@RequestBody Recipe newRecipe, @PathVariable Long id) {
 
         return recipeRepository.findById(id)
@@ -67,6 +75,8 @@ public class RecipeController {
                     recipe.setTitle(newRecipe.getTitle());
                     recipe.setDescription(newRecipe.getDescription());
                     return recipeRepository.save(recipe);
+
+                    // TODO - return ResponseEntity.ok
                 })
                 .orElseGet(() -> {
                     newRecipe.setId(id);
@@ -74,11 +84,48 @@ public class RecipeController {
                 });
     }
 
-    @DeleteMapping("/recipes/{id}")
+    @ApiOperation(value = "Delete a recipe", response = ResponseEntity.class)
+    @DeleteMapping("/{id}")
     ResponseEntity<?> deleteRecipe(@PathVariable Long id) {
         recipeRepository.deleteById(id);
 
         // Response Entity returns a 204 No Content response
         return ResponseEntity.noContent().build();
+    }
+
+    @ApiOperation(value = "Archive a recipe", response = ResponseEntity.class)
+    @DeleteMapping("/{id}/archive")
+    public ResponseEntity<ResourceSupport> archive(@PathVariable Long id) {
+        Recipe recipe = recipeRepository.findById(id)
+                .orElseThrow(() -> new RecipeNotFoundException(id));
+
+        if (recipe.getStatus() == Status.ACTIVE) {
+            recipe.setStatus(Status.ARCHIVED);
+            return ResponseEntity.ok(recipeResourceAssembler
+                    .toResource(recipeRepository.save(recipe)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new VndErrors.VndError("Method not allowed",
+                        "You cannot archive a method that is already archived."));
+    }
+
+    @ApiOperation(value = "Unarchive a recipe", response = ResponseEntity.class)
+    @PutMapping("/{id}/unarchive")
+    public ResponseEntity<ResourceSupport> unarchive(@PathVariable Long id) {
+        Recipe recipe = recipeRepository.findById(id)
+                .orElseThrow(() -> new RecipeNotFoundException(id));
+
+        if (recipe.getStatus() == Status.ARCHIVED) {
+            recipe.setStatus(Status.ACTIVE);
+            return ResponseEntity.ok(recipeResourceAssembler
+                    .toResource(recipeRepository.save(recipe)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new VndErrors.VndError("Method not allowed",
+                        "You cannot unarchive a method that is already active."));
     }
 }
